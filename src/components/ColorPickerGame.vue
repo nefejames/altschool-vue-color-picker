50d44aad77d4d81df6e28c257253977c3a43b55c
Emadamerho-Atori Nefe <nefejames@yahoo.com>
2023-02-07 21:03:32 +0100

    committed to this

---
+++ src/components/ColorPickerGame.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Color Picker Game</h1>
+
+  <div>{{ message }}</div>
+
+  <button v-for="color in colors" :key="color" @click="matchColor(color)">
+    {{ color }}
+  </button>
+</template>
+
+<script>
+import ColorPicker from "../composables/color-picker";
+
+export default {
+  setup() {
+    const { colors, message, matchColor } = ColorPicker();
+    return { colors, message, matchColor };
+  },
+};
+</script>
+
+<style></style>
